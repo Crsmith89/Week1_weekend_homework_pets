9d816e31d34510bb673132fa92bdf1737146befa
Craig Smith <craig_ag_smith@outlook.com>
2021-06-18 15:44:16 +0100

    2nd test pass

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,4 +1,11 @@
 # WRITE YOUR FUNCTIONS HERE
+# 1 - Get pet shop name
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
+
+# 2 - Get total cash
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+
 
